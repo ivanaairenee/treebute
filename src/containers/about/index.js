6d1640307c6461c5a365908f9ffdcca5272348f0
ivanairene <ivanaairenee@gmail.com>
2018-03-22 18:31:43 +0700

    fix some linting errors

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -3,15 +3,22 @@ import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-const About = props => (
-  <div>
-    <h1>About</h1>
-    <p>About us</p>
-    <button onClick={() => props.changePage()}>Go to about page via redux</button>
-  </div>
-)
 
-const mapDispatchToProps = dispatch => bindActionCreators({
+class About extends React.PureComponent {
+  render() {
+
+    return (
+      <div>
+        <h1>About</h1>
+        <p>About us</p>
+        <button onClick={() => this.props.changePage()}>Go to about page via redux</button>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators 
+({
   changePage: () => push('/about-us')
 }, dispatch)
 
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -7,25 +7,24 @@ import styled from 'styled-components';
 const Title = styled.h1`
   color: red;
 `;
-const Home = props => (
-  <div>
-    <Title>Home</Title>
-    <p>Count: {props.count}</p>
-    <p><button onClick={() => props.changePage()}>Go to about page via redux</button></p>
-  </div>
-)
 
-const mapStateToProps = state => ({
-  count: state.counter.count,
-  isIncrementing: state.counter.isIncrementing,
-  isDecrementing: state.counter.isDecrementing
-})
+class Home extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <Title>Home</Title>
+        <p>This is your home!</p>
+        <button onClick={() => this.props.changePage()}>Go to about page via redux</button>
+      </div>
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   changePage: () => push('/about-us')
 }, dispatch)
 
 export default connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(Home)
