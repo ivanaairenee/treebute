c7bf58a74326b4a31da7e23d28df56444e557e15
ivanairene <ivanaairenee@gmail.com>
2018-05-12 13:09:53 +0700

    add mainpage container

---
+++ src/containers/MainPage/style.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import axios from 'axios';
+import ExampleComponent from 'components/ExampleComponent';
+import { MainPageElement } from './style';
+
+export default class MainPage extends React.Component {
+
+}
