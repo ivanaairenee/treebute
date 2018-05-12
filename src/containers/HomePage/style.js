4556ae869013679ad36dc87622230aabf3fab5e4
ivanairene <ivanaairenee@gmail.com>
2018-05-12 22:30:34 +0700

    fix some UI bugs

--- src/containers/HomePage/style.js
+++ src/containers/HomePage/style.js
@@ -4,6 +4,7 @@ export const HomePageElement = styled.div`
   width: 100%;
   min-height: 100vh;
   display: flex;
+  justify-content: center;
   flex-wrap: wrap;
   background: ${props => props.theme.ivory};
 
